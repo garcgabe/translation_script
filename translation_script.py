8a5ed9e0d4108ae11b846f44e9fd33aebb0da3bb
garcgabe <garciagabe0033@yahoo.com>
2024-01-13 19:52:00 -0500

    git ignore; venv; structure

--- translation_script.py
+++ translation_script.py
@@ -1,1 +1,20 @@
 import translate
+
+import sys, os
+from bs4 import BeautifulSoup
+
+
+def translate(url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        html = BeautifulSoup(response.content, 'html.parser')
+        ex = html.find(re.compile(">2022-02-07 14:03</td>"))
+    else:
+        print(f"Error requesting URL: {url}\n{response.status_code}:\n{response.reason}")
+
+
+if __name__=="__main__":
+    print("please enter the URL of the article you want to translate")
+    url = input()
+    translate(url)
