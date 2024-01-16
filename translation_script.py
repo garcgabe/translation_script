3549ab5cf79f7ee648c6ecdb2e726c052b5e40e3
garcgabe <garciagabe0033@yahoo.com>
2024-01-16 09:19:34 -0500

    working in a flow

--- translation_script.py
+++ translation_script.py
@@ -1,6 +1,6 @@
 from env import DEEPL_ACCESS_KEY
 import requests
-import time, os, keyboard
+import time, os
 
 # text to speech
 from gtts import gTTS 
@@ -13,23 +13,22 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 
 
-def record_audio():
+def record_audio(seconds):
     print("recording...")
     fs = 44100  # Sample rate
-    seconds = 10  # Duration of recording
 
-    recording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    recording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
     sd.wait()  # Wait until recording is finished
     write('input.wav', fs, recording)  # Save as WAV file
+    print("finished recording")
     return
 
  
 # Whisper performs speech-to-text
 # give it output.wav
-def audio_to_text(audio_file):
-    model = whisper.load_model("base")
+def audio_to_text(audio_file, model="tiny"):
+    model = whisper.load_model(model)
     return model.transcribe("input.wav")['text']
-
 
 def text_to_speech(message: str, language = "es"):
     speech = gTTS(text=message, lang=language)
@@ -37,21 +36,29 @@ def text_to_speech(message: str, language = "es"):
     playsound('output.mp3')
 
 def main(filepath, start_time):
-    if keyboard.is_pressed('q'):
-        record_audio()
-        scanned_text = audio_to_text("output.wav")
-        print(scanned_text)
+    seconds = input("Enter number of seconds to record:")
+    try:
+        seconds = int(seconds)
+    except:
+        print("Seconds must be an integer")
+        return
+    if seconds > 2 and seconds < 15:
+        record_audio(seconds)
+        scanned_text = audio_to_text("input.wav")
+        print(f"Scanned text: {scanned_text}") 
         text_to_speech(scanned_text)
+        scanned_text = correct_text(scanned_text)
         translated = _get_translation(scanned_text)
-        print(translated)
-
-
-    if new_file:
-        text_to_translate = (filepath+new_file)
-        print(scanned_text.replace("\n", " "))
-        _get_translation(scanned_text)
+        print(f"translated: {translated}\n\n") 
 
     return time.time()
+
+def correct_text(scanned_text):
+    answer = input("Is the text correct? (y/n)")
+    if answer == "y":
+        return scanned_text
+    else:
+        return input("Enter corrected text: ")
 
 def _get_translation(scanned_text: str):
     response = requests.post(url="https://api-free.deepl.com/v2/translate", 
