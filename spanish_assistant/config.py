f0cb99230587d06b64a9cc696aa99e5e17d9b3f4
gabe garcia <garciagabe0033@yahoo.com>
2025-06-24 21:45:20 -0400

    secrets

--- spanish_assistant/config.py
+++ spanish_assistant/config.py
@@ -1,8 +1,9 @@
 import os
+import streamlit as st
 
-DEEPL_ACCESS_KEY = os.getenv("DEEPL_ACCESS_KEY")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
+DEEPL_ACCESS_KEY = os.getenv("DEEPL_ACCESS_KEY", st.secrets["DEEPL_ACCESS_KEY"])
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", st.secrets["OPENAI_API_KEY"])
+ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY", st.secrets["ELEVEN_LABS_API_KEY"])
 
 DEEPL_API_URL = "https://api-free.deepl.com/v2/translate"
 OPENAI_TEXT_MODEL = "gpt-4o-mini"
